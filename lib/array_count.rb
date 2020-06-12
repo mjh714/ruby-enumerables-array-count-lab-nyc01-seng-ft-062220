be6d4d10008b4e3299b0b6b88c723df86229725a
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-12 14:59:05 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,15 @@
 def count_strings(array)
-  array.count
+  count = 0 
+  i = 0 
+  while i < array.length 
+  if array[i].class == String
+    count += 1 
+  end
+  i += 1 
+end
+count
 end
 
 def count_empty_strings(array)
-  array.count
+  
 end
